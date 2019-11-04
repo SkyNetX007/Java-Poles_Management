a2f68145e17da543d950cd28d0474632794d67e6
unknown <1009535916@qq.com>
2019-11-04 16:18:48 +0800

    GUI

--- src/GUI/poleGrid.java
+++ src/GUI/poleGrid.java
@@ -5,20 +5,33 @@ import javax.swing.JSlider;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 
+import database.poleInfo;
+
 public class poleGrid extends JFrame {
     /**
      *
      */
     private static final long serialVersionUID = 1L;
 
-    public poleGrid(String _id, String _name, double min, double cur, double max) {
+    public JTextField id = null, name = null, max = null, min = null, current = null;
+    public JSlider slider = null;
+
+    public poleGrid(poleInfo info) {
         setLayout(new BorderLayout(5, 5));
-        getContentPane().add("NORTH", new JTextField(_id));
-        getContentPane().add("NORTH", new JTextField(_name));
-        getContentPane().add("WEST", new JTextField(String.valueOf(min)));
-        getContentPane().add("CENTER", new JTextField(String.valueOf(cur)));
-        getContentPane().add("EAST", new JTextField(String.valueOf(max)));
-        int percentage = (int) ((cur - min) / (max - min) * 100);
+        id = new JTextField(info.id);
+        name = new JTextField(info.name);
+        max = new JTextField(String.valueOf(info.min_height));
+        current = new JTextField(String.valueOf(info.current_height));
+        min = new JTextField(String.valueOf(info.min_height));
+        int percentage = (int) ((info.current_height - info.current_height) / (info.max_height - info.min_height)
+                * 100);
+
+        getContentPane().add("NORTH", new JTextField(info.id));
+        getContentPane().add("NORTH", new JTextField(info.name));
+        getContentPane().add("WEST", new JTextField(String.valueOf(info.min_height)));
+        getContentPane().add("CENTER", new JTextField(String.valueOf(info.current_height)));
+        getContentPane().add("EAST", new JTextField(String.valueOf(info.max_height)));
+
         getContentPane().add("SOUTH", new JSlider(0, 100, percentage));
 
     }
