bc18417f33c3257030a7cfe4a7a1326cbe751c00
unknown <1009535916@qq.com>
2019-11-05 11:25:31 +0800

    developing GUI functions

--- src/GUI/interfaces.java
+++ src/GUI/interfaces.java
@@ -1,11 +1,15 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 import database.poleInfo;
 
@@ -16,17 +20,47 @@ public class interfaces extends JFrame {
      */
     private static final long serialVersionUID = 1L;
 
+    private static int checkTime = 1000;
+    private int save = 0;
+
+    private boolean contentChange = false;
+    private boolean filterChange = false;
+    private boolean err = false;
+
+    private String filter = "\0";
+
     public JScrollPane jsp = null;
     public JPanel jp = new JPanel();
+    public JTextField filterBox = new JTextField();
+
+    List<poleInfo> contentList = null;
+    List<poleInfo> filterList = null;
 
     public interfaces(List<poleInfo> infoList) {
         this.setLayout(new BorderLayout());
-        for (poleInfo i : infoList) {
+        contentList = infoList;
+        for (poleInfo i : contentList) {
             jp.add(new poleGrid(i));
         }
         jsp = new JScrollPane(jp);
-        this.setContentPane(jsp);
+        filterBox.setPreferredSize(new Dimension(200, 25));
+        this.add(filterBox, "North");
+        this.add(jsp, "Center");
         pack();
+    }
+
+    public void update() {
+        if (filterBox.getText() != filter) {
+            filter = filterBox.getText();
+            filterChange = true;
+        }
+        if (filterChange) {
+            filterList.clear();
+            for (poleInfo i : contentList) {
+                if (i.name.indexOf(filter) != -1 | i.id.indexOf(filter) != -1)
+                    filterList.add(i);
+            }
+        }
     }
 
 }
--- src/test.java
+++ src/test.java
@@ -2,18 +2,23 @@ import java.util.List;
 
 import javax.swing.JFrame;
 
-import database.*;
-import GUI.*;
+import GUI.interfaces;
+import database.database;
+import database.poleInfo;
 
 public class test {
+
     public static void main(String[] args) {
         try {
             database db = new database();
             List<poleInfo> pinfo = db.getInfo();
+
             interfaces frame = new interfaces(pinfo);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
             frame.setTitle("Java-Poles_Management");
             frame.setVisible(true);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
